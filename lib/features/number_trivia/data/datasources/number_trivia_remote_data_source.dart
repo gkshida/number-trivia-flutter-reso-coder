4a87ffe905ad6288fa67efb1f780402d48927b3b
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2020-02-05 15:41:43 -0300

    Added Remote data source

---
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -0,0 +1,13 @@
+import '../models/number_trivia_model.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  /// Calls the https://numberapi.com/{number} endpoint.
+  /// 
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+
+  /// Calls the https://numbersapi.com/random endpoint.
+  /// 
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+}
